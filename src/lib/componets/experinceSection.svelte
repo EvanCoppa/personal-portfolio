b385d64f431595a9ef1bcf6932b4bbe1b1cf69ad
Evan Coppa <42395557+EvanCoppa@users.noreply.github.com>
2023-09-27 05:30:38 -0400

    added code changing to site

--- src/lib/componets/experinceSection.svelte
+++ src/lib/componets/experinceSection.svelte
@@ -1,51 +1,79 @@
 <script>
-    import { onMount } from "svelte";
-     export let active;
-    let activeSkill = 0;
-    import experienceData from "$lib/data/experience.json";
-     var experienceSkills = experienceData.experiences[active].skills;
+  import { onMount } from "svelte";
+  import { base } from "$app/paths";
+  import { marked } from "marked";
 
-    // Assuming 'experiences' is the given array
-    onMount(() => {
-        console.log();
-    });
+  export let active;
+  export let experienceData;
 
-    function handleClick(i){
-        activeSkill = i;
-        console.log(activeSkill);
-    };
+  let activeSkill = 0;
+  let skills = experienceData[active].skills;
+  let targetDiv = document.getElementById('markedContainer');
 
-    function test() {
-        alert("test");
+    // This will run whenever targetDiv changes
+    $: if (targetDiv) {
+        console.log('targetDiv changed!');
+        updateDiv();
+  }
+
+  function handleClick(i) {
+    activeSkill = i;
+    updateDiv()  }
+
+ 
+  function updateDiv() {
+
+    console.log(experienceData[active].skills[activeSkill].isCode);
+    // Select the div using its ID
+
+    // Check the condition and set the class
+    if (experienceData[active].skills[activeSkill].isCode == true) {
+        targetDiv.setAttribute('class', 'w-1/2 bg-[#2d2d2d] rounded-lg m-auto');
+    } else {
+        targetDiv.setAttribute('class', ''); // or set to some other default class if needed
     }
-    // <ExperinceSection index=0 selected={selectedSection === 0} on:click={() => selectSection(0)} class={selectedSection === 0 ? 'red' : 'inherit'} />
-    //     <ExperinceSection index=1 selected={selectedSection === 1} on:click={() => selectSection(1)} class={selectedSection === 1 ? 'red' : 'inherit'} />
-    //     <ExperinceSection index=2 selected={selectedSection === 2} on:click={() => selectSection(2)} class={selectedSection === 2 ? 'red' : 'inherit'} />
+
+    // Use marked to parse the text and set it as the innerHTML of the div
+    targetDiv.innerHTML = marked(experienceData[active].skills[activeSkill].text);
+}
+
+// Sample usage:
+// Assume experienceData, active, and activeSkill are already defined.
+// updateDiv(experienceData, active, activeSkill);
+
+
+
+
+  // <ExperinceSection index=0 selected={selectedSection === 0} on:click={() => selectSection(0)} class={selectedSection === 0 ? 'red' : 'inherit'} />
+  //     <ExperinceSection index=1 selected={selectedSection === 1} on:click={() => selectSection(1)} class={selectedSection === 1 ? 'red' : 'inherit'} />
+  //     <ExperinceSection index=2 selected={selectedSection === 2} on:click={() => selectSection(2)} class={selectedSection === 2 ? 'red' : 'inherit'} />
 </script>
 
 <div class="experienceSection my-24 flex mx-16">
-    <div class="w-1/2 flex flex-col gap-12">
-        {#each experienceSkills as skill, i}
-        <div on:mousedown={() => handleClick(i)} class=" transition-all duration-500 rounded-lg w-2/3 px-8 py-4 m-auto {i === activeSkill ? 'shadow-2xl' : 'hover:shadow-2xl'}">
-            <h4 class="text-[rebeccapurple] font-bold">{skill.name}</h4>
-            <h5>{skill.description}</h5>
-        </div>
-        
-        {/each}
+  <div class="w-1/2 flex flex-col gap-12">
+    {#each experienceData[active].skills as skill, i}
+
+      <div
+        on:mousedown={() => handleClick(i)}
+        class=" transition-all duration-500 rounded-lg w-2/3 px-8 py-4 m-auto {i ===
+        activeSkill
+          ? 'shadow-2xl'
+          : 'hover:shadow-2xl'}"
+      >
+        <h4 class="text-[rebeccapurple] font-bold">{skill.name}</h4>
+        <h5>{skill.description}</h5>
+      </div>
+    {/each}
+ 
+  </div>
+
+  <!-- {console.log(experienceData[active].skills[activeSkill].isCode)}
+
+  <div  class="{experienceData[active].skills[activeSkill].isCode == true ? 'w-1/2 bg-[#2d2d2d] rounded-lg m-auto' : ''}">
+    {marked.parse(experienceData[active].skills[activeSkill].text)}
+ </div> -->
+
+    <div id="markedContainer" bind:this={targetDiv}>
     </div>
 
-    <div class="w-1/2 bg-[#2d2d2d] rounded-lg m-auto">
-        <pre class="language-python" tabindex="0"><code class="language-python"><span class="token decorator annotation punctuation">@app<span class="token punctuation">.</span>route</span><span class="token punctuation">(</span><span class="token string">'/&lt;path:filename&gt;'</span><span class="token punctuation">)</span> 
-            <span class="token keyword">def</span> <span class="token function">markdown_to_html</span><span class="token punctuation">(</span>filename<span class="token punctuation">)</span><span class="token punctuation">:</span>    
-                md_file <span class="token operator">=</span> os<span class="token punctuation">.</span>path<span class="token punctuation">.</span>join<span class="token punctuation">(</span><span class="token string">'path/to/markdown/files'</span><span class="token punctuation">,</span> filename <span class="token operator">+</span> <span class="token string">'.md'</span><span class="token punctuation">)</span>     
-                    <span class="token keyword">if</span> <span class="token keyword">not</span> os<span class="token punctuation">.</span>path<span class="token punctuation">.</span>exists<span class="token punctuation">(</span>md_file<span class="token punctuation">)</span><span class="token punctuation">:</span>         
-                        <span class="token keyword">return</span> <span class="token string">'File not found'</span><span class="token punctuation">,</span> <span class="token number">404</span>      
-                    <span class="token keyword">with</span> <span class="token builtin">open</span><span class="token punctuation">(</span>md_file<span class="token punctuation">,</span> <span class="token string">'r'</span><span class="token punctuation">)</span> <span class="token keyword">as</span> <span class="token builtin">file</span><span class="token punctuation">:</span>        
-                        markdown_text <span class="token operator">=</span> <span class="token builtin">file</span><span class="token punctuation">.</span>read<span class="token punctuation">(</span><span class="token punctuation">)</span>         
-                        html_content <span class="token operator">=</span> markdown<span class="token punctuation">.</span>markdown<span class="token punctuation">(</span>markdown_text<span class="token punctuation">)</span>      
-            
-                    <span class="token keyword">return</span> render_template<span class="token punctuation">(</span><span class="token string">'page.html'</span><span class="token punctuation">,</span> content<span class="token operator">=</span>html_content<span class="token punctuation">)</span>
-            </code><button>Copy</button></pre>
-
-     </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,79 @@
 <script>
-    import ExperinceSection from "../lib/componets/experinceSection.svelte";
-    import experienceData from "$lib/data/experience.json";
-    var experiences = experienceData.experiences;
-    var active = 0;
+  import ExperinceSection from "../lib/componets/experinceSection.svelte";
+  import { onMount } from 'svelte';
+
+  // import experienceData from "$lib/data/experience.json";
+  // var experiences = experienceData.experiences;
+  var active = 0;
+  let experienceData;
+  
+  async function getExperienceData() {
+    try {
+      const response = await fetch("/data/experience.json");
+       return  await response.json();
+    } catch (error) {
+      console.error("There was an error!", error);
+      return await [];
+    }
+  }
+
+
+  
+
+
+	onMount(async () => {
+	    try {
+	        const result = await getExperienceData();
+            
+	        experienceData = result.experiences;  // Extracting the array from the fetched data
+  	    } catch (error) {
+	        console.error("Error occurred while fetching:", error.message);
+	    }
+	});
+
+ 
 </script>
 
 <svelte:head>
-    <title>Home</title>
+  <title>Home</title>
 </svelte:head>
 
+<div class=" relitive" style="font-family: 'IBM Plex Mono', monospace; ">
+  <div class="bg-zinc-100 pb-48 pt-16 px-16">
+    <h1 class="text-7xl font-bold text-[rebeccapurple] leading-[5rem]">
+      I'M <span class="font-light">Evan Coppa</span>, <br /> A STUDENT WEB DEVELOPER
+    </h1>
+    <h4 class="italic text-3xl">Check out my experience:</h4>
+  </div>
+
+  <div
+    class="bg-white p-8 translate-y-[-5rem] w-[75vw] max-w-[1000px] m-auto rounded-lg drop-shadow-lg flex gap-4 justify-around"
+  >
 
 
-<div class=" relitive " style="font-family: 'IBM Plex Mono', monospace; ">
-    <div class="bg-zinc-100 pb-48 pt-16 px-16">
-        <h1 class="text-7xl font-bold text-[rebeccapurple] leading-[5rem]">
-            I'M <span class="font-light">Evan Coppa</span>, <br /> A STUDENT WEB
-            DEVELOPER
-        </h1>
-        <h4 class="italic text-3xl">Check out my experience:</h4>
-    </div>
+{#if experienceData}
+  {#each experienceData as experience, i}
+      <h5
+        on:click={() => (active = i)}
+        class="hover:scale-110 tranistion-all duration-500 {active === i
+          ? 'underline'
+          : ''}"
+      >
+        {experience.title}
+      </h5>
+    {/each}
+{/if}
 
-    <div class="bg-white p-8   translate-y-[-5rem] w-[75vw] max-w-[1000px] m-auto rounded-lg drop-shadow-lg flex gap-4 justify-around">
-        {#each experiences as experience, i}
-            <h5 on:click={() => active = i} class="hover:scale-110 tranistion-all duration-500 {active === i ? 'underline' : ''}" >{experience.title}</h5>
-        {/each}
-    </div>
+   
 
-    <div class="">
-        <ExperinceSection  active="{active}" />
-    </div>
+   
+  </div>
+
+  <div class="">
+    {#if experienceData}
+     
+
+     <ExperinceSection  active="{active}" experienceData="{experienceData}" />
+{/if}
+  </div>
 </div>
